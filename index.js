8a109ddc94726dbb850763edeef32eb8f8efb65a
themordhon <romdhonm12@gmail.com>
2022-01-03 11:29:24 +0700

    get apiKey from scraperapi

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 406;
 
+const apiKey = 'c34f7f4cce932d87f42abc3b37e3ecd2';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`
+
 app.use(express.json());
 
 app.get('/', (req,res) => {
     res.send('Welcome to My API')
 })
+
 
 app.listen(PORT, () => console.log(`Server running on post ${PORT}`));
